8e0faa9b47b97a6d44d5402f4d00c772892c2590
vishnu <you@example.com>
2017-04-26 16:17:24 +0530

    A very basic server backend that can connect with hub and also runs an http server for frontend access to user

---
+++ dev_client.py
@@ -0,0 +1,15 @@
+import tornado.websocket
+from tornado import gen
+
+@gen.coroutine
+def dev_connect():
+	client = yield tornado.websocket.websocket_connect("ws://localhost:8888/dev")
+	msg = yield client.read_message()
+	print("Message is %s\n" % msg)
+	client.write_message("001")
+	while 1:
+		continue
+	#client.close()
+
+if __name__ == "__main__":
+	tornado.ioloop.IOLoop.instance().run_sync(dev_connect)
---
+++ server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import os.path
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+
+global_devlist = []
+
+class dotslash_hub:
+	def __init__(self,a,b):
+		self.id = a
+		self.conn = b
+
+class Mainhandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render("index.html", devices=global_devlist)
+
+class Devhandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		print("New device connection\n")
+		self.write_message("Server accepted connection\n")
+	
+	def on_message(self, message):
+		print("Message received %s\n" %message)
+		device = dotslash_hub(message, self)
+		global_devlist.append(device)
+
+	def on_close(self):
+		for device in global_devlist:
+			if(device.conn == self):
+				global_devlist.remove(device)
+				print("%s closed connection \n" % device.id)
+
+class Userhandler(tornado.web.RequestHandler):
+	def post(self):
+		devid = self.get_argument('device', '')
+		message = self.get_argument('message', '')
+		print("Message from user %s" %message)
+		print("Id %s\n" %devid)
+		found = 0
+		for device in global_devlist:
+			if(device.id == devid):
+				device.conn.write_message(message)
+				found = 1
+		if (found == 0):
+			print("Device not found\n")
+
+
+def main():
+	app = tornado.web.Application(
+		[
+			(r'/', Mainhandler),
+			(r'/dev', Devhandler),
+			(r'/user', Userhandler),
+		],
+		template_path = os.path.join(os.path.dirname(__file__), "templates"),
+		static_path = os.path.join(os.path.dirname(__file__), "static")
+	)
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+	main()
