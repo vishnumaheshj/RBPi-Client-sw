9dd798a94edd3112a99e77a007c6ffdbe9a6284e
vishnu <you@example.com>
2017-05-10 18:38:02 +0530

    Client python program initiates processing of datasendrcv.bin in a separate thread.Exits is ZNP init reports failure else connects to server

--- dev_client.py
+++ dev_client.py
@@ -1,21 +1,56 @@
 import tornado.websocket
 from tornado import gen
 import ctypes
-from ctypes import cdll
+import subprocess
+from threading import Thread
+
+#Global variable to ensure that client connects to server only after successfully initializing ZNP 
+binary_init_status = 0
+
+#Method to run ZNP init binary
+def execute_binary():
+	global binary_init_status
+	binary = subprocess.Popen("./dataSendRcv.bin", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	while binary.poll() is None:
+		line =  binary.stdout.readline()
+		if line != '':
+			if "Error" in line:
+				print(line)
+				binary_init_status = -1
+				binary.terminate()
+				break
+			if "Enter message" in line:
+				binary_init_status = 1
 
 @gen.coroutine
 def dev_connect():
-	lib = cdll.LoadLibrary("/home/dotslash/dotslash/tornado_backend/shared/libshm.so")
-	id = lib.init_shm()
+	#shm related initializations
+	lib = ctypes.cdll.LoadLibrary("/home/dotslash/dotslash/tornado_backend/shared/libshm.so")
+	shm_id = lib.init_shm()
+
+	#Execute dataSendRcv binary on separate thread
+	thread = Thread(target = execute_binary)
+	thread.daemon = True
+    	thread.start()
+	print("Initializing ZNP")
+	while binary_init_status== 0:
+		continue
+	if binary_init_status== -1:
+		print("Could not initialize ZNP device")
+		return
+	else:
+		print("ZNP init success, connecting to server")
+
+	#Connect to server
 	#client = yield tornado.websocket.websocket_connect("ws://192.168.0.106:8888/dev")
 	client = yield tornado.websocket.websocket_connect("ws://localhost:8888/dev")
 	msg = yield client.read_message()
-	print("Message is %s\n" % msg)
+	print("%s\n" % msg)
 	client.write_message("001")
 	while 1:
 		msg = yield client.read_message()
-		print("Message is %s\n" % msg)
-		lib.update_shm(ctypes.create_string_buffer(msg), id)
+		print("Server Message is %s\n" % msg)
+		lib.update_shm(ctypes.create_string_buffer(msg), shm_id)
 		continue
 	client.close()
 
