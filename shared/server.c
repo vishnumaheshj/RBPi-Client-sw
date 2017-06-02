085c1856528e74fd795d3b45e0be4978a60b1650
Vishnu <vishnu@dot-slash.in>
2017-06-03 00:34:59 +0530

    Replacing content of library functions (APIs) with message queue

--- shared/server.c
+++ shared/server.c
@@ -16,19 +16,181 @@
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <sys/types.h>
+#include  <sys/types.h>
 #include  <sys/ipc.h>
-#include  <sys/shm.h>
+#include  <sys/msg.h>
 #include  <unistd.h>
 #include  <string.h>
 #include "switchboard.h"
 
+struct msgq_buf {
+    long mtype;
+    char mtext[256];
+};
+
+
+int init_write_shm()
+{
+    key_t          MsgQKEY;
+    int            MsgQID = -1;
+
+    if ((MsgQKEY = ftok("/home", 'y')) == -1) {
+        perror("ftok");
+        return -1; // Need to add and handle proper error code.
+    }
+    if ((MsgQID = msgget(MsgQKEY, 0666)) == -1) {
+        perror("msgget");
+        return -1; // Need to add and handle proper error code.
+    }
+
+    return MsgQID;
+}
+
+int init_read_shm()
+{
+    key_t          MsgQKEY;
+    int            MsgQID = -1;
+
+    if ((MsgQKEY = ftok("/home", 'x')) == -1) {
+        perror("ftok");
+        return -1; // Need to add and handle proper error code.
+    }
+    if ((MsgQID = msgget(MsgQKEY, 0666)) == -1) {
+        perror("msgget");
+        return -1; // Need to add and handle proper error code.
+    }
+
+    return MsgQID;
+}
+
+int is_rbuf_ready_nw(int MsgQID)
+{
+    struct msqid_ds status;
+    if(msgctl(MsgQID, MSG_STAT, &status) == -1) {
+        perror("msgctl");
+        return -1; // Need to add and handle proper error code
+    }
+    if(status.msg_qnum == 0)
+        return 0;
+    else
+        return 1;
+
+}
+
+int read_shm(char *data, int MsgQID)
+{
+
+    struct msgq_buf buf;
+    buf.mtype = 0;
+    int dataSize;
+    sbMessage_t *sMsg;
+
+    dataSize = sizeof(sbMessage_t);
+    if (msgrcv(MsgQID, &buf, dataSize, 0, 0) == -1) {
+        perror("msgrcv");
+        return -1;
+    }
+
+    sMsg = (sbMessage_t *)buf.mtext;
+
+
+    if (sMsg->hdr.message_type == SB_BOARD_INFO_RSP)
+    {
+        printf("r:board info rsp\n");
+    }
+    else if (sMsg->hdr.message_type == SB_STATE_CHANGE_RSP)
+    {
+        printf("r:state change rsp\n");
+    }
+    else if (sMsg->hdr.message_type == SB_DEVICE_READY_NTF)
+    {
+        printf("r:device ready notification\n");
+    }
+    else if (sMsg->hdr.message_type == SB_DEVICE_INFO_NTF)
+    {
+        printf("r:dev info message.\n");
+        printf("C:::r:sizeof sMsg:%lu\n", sizeof(sbMessage_t));
+        
+        printf("r: message type:%x\n", sMsg->hdr.message_type);
+        printf("r: join state  :%x\n", sMsg->data.devInfo.joinState);
+        printf("r: sbType      :%x\n", sMsg->data.devInfo.sbType.type);
+        printf("r: dev index   :%x\n", sMsg->data.devInfo.devIndex);
+        printf("r: ieee addr   :%lx\n", sMsg->data.devInfo.ieeeAddr);
+        printf("r: ep status   :%x\n", sMsg->data.devInfo.epStatus);
+        printf("r: switch1     :%x\n", sMsg->data.devInfo.currentState.switch1);
+        printf("r: switch2     :%x\n", sMsg->data.devInfo.currentState.switch2);
+        printf("r: switch3     :%x\n", sMsg->data.devInfo.currentState.switch3);
+        printf("r: switch4     :%x\n", sMsg->data.devInfo.currentState.switch4);
+        printf("r: switch5     :%x\n", sMsg->data.devInfo.currentState.switch5);
+        printf("r: switch6     :%x\n", sMsg->data.devInfo.currentState.switch6);
+        printf("r: switch7     :%x\n", sMsg->data.devInfo.currentState.switch7);
+        printf("r: switch8     :%x\n", sMsg->data.devInfo.currentState.switch8);
+    }
+    else
+    {
+        printf("r:unknown message\n");
+    }
+
+    memcpy(data, buf.mtext, dataSize);
+    return dataSize;
+}
+
+int write_shm(char *data, int MsgQID)
+{
+
+    struct msgq_buf buf;
+    int dataSize;
+
+    buf.mtype = 1;
+    sbMessage_t *sMsg = (sbMessage_t *)data;
+    
+    dataSize = sizeof(sbMessage_t);
+
+    if (sMsg->hdr.message_type == SB_BOARD_INFO_REQ)
+    {
+        printf("board info req\n");
+    }
+    else if (sMsg->hdr.message_type == SB_STATE_CHANGE_REQ)
+    {
+        printf("w:state change req\n");
+    }
+    else if (sMsg->hdr.message_type == SB_DEVICE_READY_REQ)
+    {
+        printf("w:device ready req\n");
+    }
+    else
+    {
+        printf("w:unknown message\n");
+    }
+
+    memset(buf.mtext, 0, 256);
+    memcpy(buf.mtext, data, dataSize);
+    if (msgsnd(MsgQID, &buf, dataSize, 0) == -1) {
+        perror("msgsnd");
+        return -1;
+    }
+    return dataSize;
+}
+
+
+int delete_shm(int MsgQID)
+{
+    if (msgctl(MsgQID, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        return -1; // Need to add and handle proper error code
+    }
+    return 0;
+}
+
+#if 0
 #define  NOT_READY  -1
 #define  FILLED     1
 #define  TAKEN      0
 
+
 struct Memory {
-	int  status;
-	char data[256];
+    int  status;
+    char data[256];
 };
 
 int init_write_shm()
@@ -66,20 +228,20 @@ int is_rbuf_ready_nw(int ShmID)
     ShmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
     if (ShmPTR == NULL)
     {
-	    ret = 0;
+        ret = 0;
     }
-	else if (ShmPTR->status == FILLED)
+    else if (ShmPTR->status == FILLED)
     {
-	    ret = 1;
+        ret = 1;
     }
-	else
+    else
     {
-	    ret = 0;
+        ret = 0;
     }
 
-	shmdt((void *) ShmPTR);
+    shmdt((void *) ShmPTR);
 
-	return ret;
+    return ret;
 }
 
 int read_shm(char *data, int ShmID)
@@ -91,7 +253,7 @@ int read_shm(char *data, int ShmID)
     ShmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
 
     if (ShmPTR == NULL)
-	    return -1;
+        return -1;
 
     printf("r:waiting to be filled filled\n");
     while (ShmPTR->status != FILLED)
@@ -100,7 +262,7 @@ int read_shm(char *data, int ShmID)
 
     sMsg = (sbMessage_t *)ShmPTR->data;
 
-	dataSize = sizeof(sbMessage_t);
+    dataSize = sizeof(sbMessage_t);
 
     if (sMsg->hdr.message_type == SB_BOARD_INFO_RSP)
     {
@@ -116,9 +278,9 @@ int read_shm(char *data, int ShmID)
     }
     else if (sMsg->hdr.message_type == SB_DEVICE_INFO_NTF)
     {
-	    printf("r:dev info message.\n");
+        printf("r:dev info message.\n");
         printf("C:::r:sizeof sMsg:%lu\n", sizeof(sbMessage_t));
-		
+        
         printf("r: message type:%x\n", sMsg->hdr.message_type);
         printf("r: join state  :%x\n", sMsg->data.devInfo.joinState);
         printf("r: sbType      :%x\n", sMsg->data.devInfo.sbType.type);
@@ -155,9 +317,9 @@ int write_shm(char *data, int ShmID)
     ShmPTR = (struct Memory *) shmat(ShmID, NULL, 0);
 
     if (ShmPTR == NULL)
-	    return -1;
+        return -1;
 
-	dataSize = sizeof(sbMessage_t);
+    dataSize = sizeof(sbMessage_t);
     if (sMsg->hdr.message_type == SB_BOARD_INFO_REQ)
     {
         printf("board info req\n");
@@ -189,7 +351,7 @@ int delete_shm(int ShmID)
     shmctl(ShmID, IPC_RMID, NULL);
     return 0;
 }
-
+#endif
 
 #if 0
 void  main(int  argc, char *argv[])
