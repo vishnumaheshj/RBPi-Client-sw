474c2120349c6c5e56ccae8fd53de27515b9cfe9
Pradul MT <pradul@dotslash.in>
2017-06-09 01:29:06 +0530

    Handling ajax response and updating switch states in browser.

--- serverDB.py
+++ serverDB.py
@@ -9,6 +9,7 @@ hubStates = None
 hubUsers = None
 connectionList = bidict()
 
+
 def initDatabase():
     global hubCollection
     global hubStates
@@ -20,11 +21,6 @@ def initDatabase():
     hubStates = db.hubStates
     hubUsers = db.hubUsers
 
-    # REMOVE
-#    hubCollection.drop()
-#    hubStates.drop()
-#    hubUsers.drop()
-    # Debug
     print("Database init success")
     print ("hubCollection entries:%d" % hubCollection.count())
     print ("hubStates entries:%d" % hubStates.count())
@@ -38,7 +34,6 @@ def addHub(connection, clientMessage):
     if clientMessage['message_type'] != SB_DEVICE_READY_NTF:
         return 1
 
-    #connectionList(connection) = clientMessage['hubAddr']
     connectionList[clientMessage['hubAddr']] = connection
     
     cursor = hubCollection.find_one({"hubAddr": clientMessage['hubAddr']})
@@ -76,6 +71,7 @@ def addHub(connection, clientMessage):
     print("connection list")
     print(connectionList)
     print("###############################################################")
+
 
 def addHubStates(clientMessage, connection):
     
@@ -168,6 +164,7 @@ def updateNode(connection, clientMessage):
                     }
             })
 
+
 def findNode(hubAddr, nodeid):
     boardStr = "board"+str(nodeid)
     row = hubStates.find_one({"hubAddr": hubAddr, boardStr+".devIndex": nodeid})
@@ -176,6 +173,7 @@ def findNode(hubAddr, nodeid):
             return node
     else:
             return None
+
 
 def findHub(hubAddr):
     return hubStates.find_one({"hubAddr": hubAddr})
@@ -194,6 +192,7 @@ def makeHubOffline(hubAddr):
             print("document")
             print(document)
             print("'''''''''''''''")
+
 
 def addUser(username, password):
     cursor = hubUsers.find_one({"username": username})
@@ -269,6 +268,7 @@ def logoutUser(username):
 
     return status
 
+
 def registerHub(username, hubId):
     cursor = hubUsers.find_one({"username": username})
     if cursor is not None:
@@ -292,7 +292,7 @@ def registerHub(username, hubId):
                 }
             )
  
-            status = None;
+            status = None
     else:
         print("Signup failed.Hub not registered")
         status = "Signup failed.Hub not registered"
@@ -303,6 +303,7 @@ def registerHub(username, hubId):
         print(hubsDB)
 
     return status
+
 
 def findUserHub(username):
     cursor = hubUsers.find_one({"username": username})
--- static/dist/js/site.js
+++ static/dist/js/site.js
@@ -3,7 +3,7 @@
  */
 
 /*
-{"_id": {"$oid": "5938405d23ea620c8afa93f4"}, "hubAddr": 72623859790382856, "board1": {"switch8": 4, "devIndex": 1, "switch3": 0, "lastModified": {"$date": 1496953485725}, "switch1": 0, "switch7": 4, "switch6": 4, "switch5": 4, "switch4": 0, "epStatus": 5, "switch2": 0, "type": 2}, "totalNodes": 1}
+{"_id": {"$oid": "5938405d23ea620c8afa93f4"}, "hubAddr": 72623859790382856, "board1": {"switch8": 4, "devIndex": 1, "switch3": 1, "lastModified": {"$date": 1496953485725}, "switch1": 1, "switch7": 4, "switch6": 4, "switch5": 4, "switch4": 0, "epStatus": 5, "switch2": 0, "type": 2}, "totalNodes": 1}
 */
 $("[id^=user_msg]").submit(function(e) {
 
@@ -14,8 +14,27 @@ $("[id^=user_msg]").submit(function(e) {
            success: function(data)
            {
 				alert(data); // show response from the server.
-				var json = JSON.parse(data);
-				//if (json["switch1"]
+				var data1 = '{"_id": {"$oid": "5938405d23ea620c8afa93f4"}, "hubAddr": 72623859790382856, "board1": {"switch8": 4, "devIndex": 1, "switch3": 1, "lastModified": {"$date": 1496953485725}, "switch1": 1, "switch7": 4, "switch6": 4, "switch5": 4, "switch4": 0, "epStatus": 5, "switch2": 0, "type": 2}, "totalNodes": 1}'
+				var json = JSON.parse(data1);
+				if (typeof(json["hubAddr"]) != 'undefined')
+				{
+				    var index;
+				    var switches = ["switch1", "switch2", "switch3", "switch4", "switch5", "switch6", "switch7", "switch8"];
+				    for (index = 0; index < switches.length; index++)
+				    {
+				        if (typeof(json.board1[switches[index]]) != undefined)
+                        {
+                            if (json.board1[switches[index]] == 0x1)
+                            {
+                                $('#' + switches[index]).bootstrapToggle('on')
+                            }
+                            else if (json.board1[switches[index]] == 0x0)
+                            {
+                                $('#' + switches[index]).bootstrapToggle('off')
+                            }
+                        }
+				    }
+				}
            }
          });
 
