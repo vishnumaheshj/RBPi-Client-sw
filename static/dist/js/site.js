71f9fb719400365bfe59c98412d20ffb4ca3f4cb
Pradul MT <pradul@dotslash.in>
2017-06-09 13:37:50 +0530

    Handling ajax response: Update all boards from the response.

--- static/dist/js/site.js
+++ static/dist/js/site.js
@@ -7,37 +7,47 @@
 */
 $("[id^=user_msg]").submit(function(e) {
 
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
     $.ajax({
            type: $(this).attr('method'),
            url:  $(this).attr('action'),
            data: $(this).serialize(), // serializes the form's elements.
            success: function(data)
            {
-				alert(data); // show response from the server.
+				// alert(data); // show response from the server.
 				var data1 = '{"_id": {"$oid": "5938405d23ea620c8afa93f4"}, "hubAddr": 72623859790382856, "board1": {"switch8": 4, "devIndex": 1, "switch3": 1, "lastModified": {"$date": 1496953485725}, "switch1": 1, "switch7": 4, "switch6": 4, "switch5": 4, "switch4": 0, "epStatus": 5, "switch2": 0, "type": 2}, "totalNodes": 1}'
-				var json = JSON.parse(data1);
-				if (typeof(json["hubAddr"]) != 'undefined')
+				var jsonData = JSON.parse(data1);
+				if (typeof(jsonData["hubAddr"]) != 'undefined')
 				{
 				    var index;
+				    var i;
 				    var switches = ["switch1", "switch2", "switch3", "switch4", "switch5", "switch6", "switch7", "switch8"];
-				    for (index = 0; index < switches.length; index++)
-				    {
-				        if (typeof(json.board1[switches[index]]) != undefined)
+
+                    for (board in jsonData)
+                    {
+                        if (board.substring(0,5) == 'board')
                         {
-                            if (json.board1[switches[index]] == 0x1)
+                            for (index = 0; index < switches.length; index++)
                             {
-                                $('#' + switches[index]).bootstrapToggle('on')
-                            }
-                            else if (json.board1[switches[index]] == 0x0)
-                            {
-                                $('#' + switches[index]).bootstrapToggle('off')
+                                if (typeof(jsonData[board][switches[index]]) != undefined)
+                                {
+                                    if (jsonData[board][switches[index]] == 0x1)
+                                    {
+                                        // TODO: Check if such board/switch exist. if not, reload..
+                                        $('#' + board + switches[index]).bootstrapToggle('on')
+                                    }
+                                    else if (jsonData[board][switches[index]] == 0x0)
+                                    {
+                                        // TODO: Check if such board/switch exist. if not, reload..
+                                        $('#' + board + switches[index]).bootstrapToggle('off')
+                                    }
+                                }
                             }
                         }
-				    }
+                    }
 				}
            }
          });
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
 });
 
